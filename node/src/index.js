8d526a72cacda974f53b1cec9a87ada30ec470ef
melissaoalves <msnmelissaoa15@hotmail.com>
2025-07-10 20:48:22 -0300

    implement body parser helper

--- node/src/index.js
+++ node/src/index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const bodyParser = require('./helpers/bodyParser');
 
 const { URL } = require("url");
 
@@ -33,7 +34,11 @@ const server = http.createServer((request, response) => {
       response.end(JSON.stringify(body));
     }
 
-    route.handler(request, response);
+    if (['POST', 'PUT', 'PATCH'].includes(request.method)){
+      bodyParser(request, ()=> route.handler(request, response));
+    } else{
+      route.handler(request, response);
+    }
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
